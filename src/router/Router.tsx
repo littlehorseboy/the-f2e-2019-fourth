a4466646723e6fe556b6414ae155488d05f8b8b7
littlehorseboy <littlehorseboy@gmail.com>
2019-08-06 00:33:48 +0800

    feat: router CSSTransition 搬得太高 需要搞 nested 處理

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import { CSSTransition } from 'react-transition-group';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import PaymentPage from '../pages/PaymentPage/PaymentPage';
+
+const bannerImg = require('../assets/images/Photo by Xianjuan HU on Unsplash.png'); // eslint-disable-line @typescript-eslint/no-var-requires
+const brandImg = require('../assets/images/Group 167.png'); // eslint-disable-line @typescript-eslint/no-var-requires
+
+const routes = [
+  { path: '/', name: 'Home', Component: PaymentPage },
+  { path: '/payment/:type', name: 'paymentType', Component: PaymentPage },
+  { path: '/payment', name: 'payment', Component: PaymentPage },
+];
 
 const useStyles = makeStyles({
   page: {
     fontFamily: 'system-ui, -apple-system, "Roboto", "Helvetica", "Arial", sans-serif',
-    transition: 'opacity 250ms ease-in',
+  },
+  root: {
+    width: '100%',
+    minHeight: '100vh',
+    backgroundColor: '#343434',
+  },
+  leftPanel: {
+    backgroundColor: '#A8D574',
+  },
+  leftPanelWrapper: {
+    paddingTop: 56,
+    paddingLeft: 136,
+  },
+  bannerImg: {
+    width: '100%',
+  },
+  titleContent: {
+    fontSize: 40,
+    paddingBottom: 24,
+  },
+  fieldName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  fieldValue: {
+    fontSize: 24,
+    fontWeight: 'lighter',
+    paddingBottom: 16,
+  },
+  fade: {
+    transition: 'opacity 0.7s ease-in',
     '&-enter': {
-      opacity: 0.5,
+      opacity: 0,
     },
     '&-enter-done': {
       opacity: 1,
@@ -17,7 +61,7 @@ const useStyles = makeStyles({
       opacity: 1,
     },
     '&-exit-done': {
-      opacity: 0.5,
+      opacity: 0,
     },
   },
 });
@@ -28,9 +72,61 @@ export default function Router(): JSX.Element {
   return (
     <HashRouter>
       <div className={classes.page}>
-        <Route path='/' exact component={PaymentPage} />
-        <Route path='/payment/:type' component={PaymentPage} />
-        <Route path='/payment' exact component={PaymentPage} />
+        <div className={classes.root}>
+          <Container>
+            <Grid container>
+              <Grid item xs={12} sm={5} className={classes.leftPanel}>
+                <img src={bannerImg} alt="payment" className={classes.bannerImg} />
+
+                <div className={classes.leftPanelWrapper}>
+                  <div className={classes.titleContent}>訂單資訊</div>
+                  <div className={classes.fieldName}>商店名稱</div>
+                  <div className={classes.fieldValue}>Amazing 3C online Shop</div>
+                  <div className={classes.fieldName}>訂單編號</div>
+                  <div className={classes.fieldValue}>239234dwnd321</div>
+                  <div>本筆訂單將支付</div>
+                  <div>239234dwnd321</div>
+                  <div>
+                    {(1250).toLocaleString()}
+                    <span>元</span>
+                  </div>
+                  <div>支付方式</div>
+                  <div>信用卡</div>
+                  <div>
+                    <span>網路ATM</span>
+                    <span>(晶片讀卡機轉帳)</span>
+                  </div>
+                  <div>
+                    <span>ATM櫃員機</span>
+                    <span>(實體ATM及網銀)</span>
+                  </div>
+                  <div>信用卡</div>
+                </div>
+
+                <img src={brandImg} alt="payment" />
+              </Grid>
+
+              <Grid item xs={12} sm={7}>
+                {routes.map(({ path, Component }): JSX.Element => (
+                  <Route key={path} exact path={path}>
+                    {({ match }): JSX.Element => (
+                      <CSSTransition
+                        in={match ? match.params.type === 'creditCard' : false}
+                        timeout={300}
+                        classNames={classes.fade}
+                        unmountOnExit
+                      >
+                        <div className={classes.fade}>
+                          <Component />
+                        </div>
+                      </CSSTransition>
+                    )}
+                  </Route>
+                ))}
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
       </div>
     </HashRouter>
   );
